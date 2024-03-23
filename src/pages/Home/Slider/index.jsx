591b35c898e51aae50714bc17be51fcf9ab2535f
Zhazgul Arystanbek kyzy <zhazgul.arystanbekkyzy.kk@gmail.com>
2024-03-23 11:46:22 +0300

    added files

---
+++ src/pages/Home/Slider/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Slider = () => {
+  const sliderImg = [
+    "https://staticcontent.mypizza.kg/Promotions/COCA-COLA_V_PODAROK/Large.png?hash=7ed60b811ea2ff4b8d928533ea688dde&quot",
+    ""
+  ];
+  return (
+    <div id="slider">
+      <div className="container">
+        <div className="slider"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
+<div class="news-banner" style="background-image: url(&quot;https://staticcontent.mypizza.kg/Promotions/Aktsiia_Afterparty/Large.png?hash=3ff831fec69684b937136a8828ca846b&quot;);"><h1 class="news-banner_title">
+            null
+          </h1></div>
+  <h1 class="news-banner_title">null</h1>
+</div>;
